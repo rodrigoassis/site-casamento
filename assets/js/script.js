00956b394a676b725c2551f4061a9a1e448980fb
Rodrigo Assis <rodrigo.assis@reinteractive.com>
2023-02-21 16:42:41 -0300

    primeiro blog de hospedagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -273,12 +273,8 @@
     function preloader() {
         if($('.preloader').length) {
             $('.preloader').delay(100).fadeOut(500, function() {
-
                 //active wow
                 wow.init();
-
-
-
             });
         }
     }
